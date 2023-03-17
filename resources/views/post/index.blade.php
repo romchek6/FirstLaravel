266340c4a8da64c4ccfac349697307cc5ff100e3
veden <stud0000248921@study.utmn.ru>
2023-03-17 14:17:07 +0500

    second commit

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 @section('content')
+    <a href="/post/create">Добавить</a>
     @foreach($posts as $key=>$value)
 
         <a href="/post/{{$value->id}}">{{ $value->title }}</a>
--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 @section('content')
+    <a href="/tag/create">Добавить</a>
     @foreach($tags as $key=>$value)
 
         <a href="/tag/{{$value->id}}">{{ $value->title }}</a>
